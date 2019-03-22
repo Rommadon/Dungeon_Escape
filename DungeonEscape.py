469953a57be2988f7b8f1134b37f79e9e632ade8
Rom <rommadon.t@appman.co.th>
2019-03-22 13:41:17 +0700

    Create background img

--- DungeonEscape.py
+++ DungeonEscape.py
@@ -8,26 +8,14 @@ SCREEN_TITLE = "Dungeon Escape"
 class DungeonEscape(arcade.Window):
   def __init__(self, width, height, title):
     super().__init__(width, height, title)
-
-    arcade.set_background_color(arcade.color.AMAZON)
-
-    # If you have sprite lists, you should create them here,
-    # and set them to None
+    self.background = None
 
   def setup(self):
-    # Create your sprites and sprite lists here
-    pass
+    self.background = arcade.load_texture("images/background.jpg")
 
   def on_draw(self):
-    """
-    Render the screen.
-    """
-
-    # This command should happen before we start drawing. It will clear
-    # the screen to the background color, and erase what we drew last frame.
     arcade.start_render()
-
-    # Call draw() on all your sprite lists below
+    arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
   def update(self, delta_time):
     """
@@ -53,7 +41,7 @@ class DungeonEscape(arcade.Window):
     pass
 
 def main():
-  game = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+  game = DungeonEscape(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
   game.setup()
   arcade.run()
 
